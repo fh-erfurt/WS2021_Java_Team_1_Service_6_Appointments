28f60db690ab06e7763f799b615f006c4e1e38ac
JHJojo <70706428+JHJojo@users.noreply.github.com>
2022-03-23 23:37:32 +0100

    updated appointment sorting

--- appointments-service/src/main/java/de/fherfurt/appointments/sorting/SortBy.java
+++ appointments-service/src/main/java/de/fherfurt/appointments/sorting/SortBy.java
@@ -1,4 +1,39 @@
 package de.fherfurt.appointments.sorting;
 
+import de.fherfurt.appointments.main.Appointment;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class SortBy {
+
+    List<Appointment> inputAppointmentList;
+
+    public SortBy(List<Appointment> appointmentList) {
+        this.inputAppointmentList = appointmentList;
+    }
+
+    public List<Appointment> SortByDate(){
+      inputAppointmentList.sort(Comparator.comparing(Appointment::getDate));
+
+      return inputAppointmentList;
+    }
+
+    public List<Appointment> SortByCreatorName(){
+        inputAppointmentList.sort((Appointment a1, Appointment a2) -> {
+            return a1.getCreator().getName().compareToIgnoreCase(a2.getCreator().getName());
+        });
+
+        return inputAppointmentList;
+    }
+
+    public List<Appointment> SortByCampus(){
+        inputAppointmentList.sort((Appointment a1, Appointment a2) -> {
+            return a1.getCampusLocation().compareTo(a2.getCampusLocation());
+        });
+
+        return inputAppointmentList;
+    }
 }
