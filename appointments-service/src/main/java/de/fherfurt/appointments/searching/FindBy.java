ea696584ba179f8334fe09a7f533e925503d51b3
JHJojo <70706428+JHJojo@users.noreply.github.com>
2022-03-23 01:09:07 +0100

    updated appointment filter

--- appointments-service/src/main/java/de/fherfurt/appointments/searching/FindBy.java
+++ appointments-service/src/main/java/de/fherfurt/appointments/searching/FindBy.java
@@ -2,8 +2,11 @@ package de.fherfurt.appointments.searching;
 
 
 import de.fherfurt.appointments.main.Appointment;
+import de.fherfurt.appointments.main.Campus;
+import de.fherfurt.appointments.models.Creator;
 
 import javax.swing.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -11,16 +14,48 @@ import java.util.stream.Collectors;
 
 public class FindBy {
 
+    List<Appointment> inputAppointment;
+
+
     public FindBy(List<Appointment> appointmentList) {
+        this.inputAppointment = appointmentList;
     }
 
-    public List<Appointment> FindByName (List<Appointment> appointmentList, String inputName){
+    public List<Appointment> FindById (int inputId){
 
-        List<Appointment> filteredAppointment = appointmentList.stream()
+        return inputAppointment.stream()
+                .filter(appointment -> appointment.getId() == inputId)
+                .collect(Collectors.toList());
+    }
+
+    public List<Appointment> FindByName (String inputName){
+
+        return inputAppointment.stream()
                 .filter(appointment -> appointment.getName().startsWith(inputName))
                 .collect(Collectors.toList());
-
-        return filteredAppointment;
     }
+
+    public List<Appointment> FindByDate (LocalDateTime inputDate){
+
+        return inputAppointment.stream()
+                .filter(appointment -> appointment.getDate().equals(inputDate))
+                .collect(Collectors.toList());
+    }
+
+    public List<Appointment> FindByCreator (Creator inputCreator){
+
+        return inputAppointment.stream()
+                .filter(appointment -> appointment.getCreator().equals(inputCreator))
+                .collect(Collectors.toList());
+    }
+
+    public List<Appointment> FindByCampus (Campus inputCampus){
+
+        return inputAppointment.stream()
+                .filter(appointment -> appointment.getCampusLocation().equals(inputCampus))
+                .collect(Collectors.toList());
+    }
+
+
 
 }
