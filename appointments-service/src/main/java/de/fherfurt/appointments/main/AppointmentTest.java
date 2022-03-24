4721046138679c99a917ba9b66a5dc6eb9eec200
Nadine Huetter <nadine.huetter@fh-erfurt.de>
2022-03-24 13:56:30 +0100

    Unit tests Teil4

--- appointments-service/src/main/java/de/fherfurt/appointments/main/AppointmentTest.java
+++ appointments-service/src/main/java/de/fherfurt/appointments/main/AppointmentTest.java
@@ -2,8 +2,10 @@ package de.fherfurt.appointments.main;
 
 import de.fherfurt.appointments.models.Professor;
 import de.fherfurt.appointments.models.Student;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,22 +13,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppointmentTest {
 
     Professor TestProfessor = new Professor("Test","12345");
-
+    LocalDateTime TestTime = LocalDateTime.now();
     @Test
     void TestaddParticipant() {
         //given
-            Appointment TestAppointment = new Appointment("Test",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+            Appointment TestAppointment = new Appointment("Test",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
             Student RandomName = new Student("Random Name","12345");
         //when
             TestAppointment.addParticipant(RandomName);
         //then
-            Assert.assertEquals(TestAppointment.getParticipants(), Arrays.asList("RandomName"));
+            Assertions.assertEquals(TestAppointment.getParticipants(), Arrays.asList("RandomName"));
     }
 
     @Test
     void TestunsignedParticipant_Participantissigned() {
         //given
-        Appointment TestAppointment = new Appointment("Test",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment = new Appointment("Test",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
         Student RandomName = new Student("Random Name","12345");
         Student Studenttounsign = new Student("Tobe Deleted","12345");
         TestAppointment.addParticipant(RandomName);
@@ -34,20 +36,20 @@ class AppointmentTest {
         //when
         TestAppointment.unsignedParticipant(Studenttounsign);
         //then
-        Assert.assertEquals(TestAppointment.getParticipants(), Arrays.asList("RandomName"));
+        Assertions.assertEquals(TestAppointment.getParticipants(), Arrays.asList("RandomName"));
     }
 
     @Test
     void TestunsignedParticipant_ParticipantisNOTsigned() {
         //given
-        Appointment TestAppointment = new Appointment("Test",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment = new Appointment("Test",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
         Student RandomName = new Student("Random Name","12345");
         Student Studenttounsign = new Student("Tobe Deleted","12345");
         TestAppointment.addParticipant(RandomName);
         //when
         TestAppointment.unsignedParticipant(Studenttounsign);
         //then
-        Assert.assertEquals(TestAppointment.getParticipants(), Arrays.asList("RandomName"));
+        Assertions.assertEquals(TestAppointment.getParticipants(), Arrays.asList("RandomName"));
     }
 
     /*@Test --> nicht benötigt weil delAppointmentwird getestet(funktion ruft nur delAppointment auf
--- appointments-service/src/main/java/de/fherfurt/appointments/models/CreatorTest.java
+++ appointments-service/src/main/java/de/fherfurt/appointments/models/CreatorTest.java
@@ -4,80 +4,83 @@ import de.fherfurt.appointments.main.Appointment;
 import de.fherfurt.appointments.main.Campus;
 import de.fherfurt.appointments.main.Repetition;
 import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreatorTest {
 
+    LocalDateTime TestTime = LocalDateTime.now();
     Professor TestProfessor = new Professor("Test","12345");
     @Test
     void TestaddONEAppointment() {
         //given
-        Appointment TestAppointment = new Appointment("Test",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment = new Appointment("Test",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
         Student RandomName = new Student("Random Name","12345");
         //when
         RandomName.addAppointment(TestAppointment);
         //then
-        Assert.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment"));
+        Assertions.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment"));
     }
     @Test
     void TestaddSEVERALAppointments() {
         //given
-        Appointment TestAppointment1 = new Appointment("Test1",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
-        Appointment TestAppointment2 = new Appointment("Test2",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
-        Appointment TestAppointment3 = new Appointment("Test3",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment1 = new Appointment("Test1",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment2 = new Appointment("Test2",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment3 = new Appointment("Test3",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
         Student RandomName = new Student("Random Name","12345");
         //when
         RandomName.addAppointment(TestAppointment1,TestAppointment2,TestAppointment3);
         //then
-        Assert.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment1","TestAppointment2","TestAppointment3"));
+        Assertions.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment1","TestAppointment2","TestAppointment3"));
     }
 
     @Test
     void TestdelONEAppointment() {
         //given
-        Appointment TestAppointment1 = new Appointment("Test1",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
-        Appointment TestAppointment2 = new Appointment("Test2",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment1 = new Appointment("Test1",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment2 = new Appointment("Test2",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
         Student RandomName = new Student("Random Name","12345");
         RandomName.addAppointment(TestAppointment1,TestAppointment2);
         //when
         RandomName.delAppointment(TestAppointment1);
         //then
-        Assert.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment2"));
+        Assertions.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment2"));
 
     }
     @Test
     void TestdelSEVERALAppointments_OneAppointmentunsigned() {
         //given
-        Appointment TestAppointment1 = new Appointment("Test1",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
-        Appointment TestAppointment2 = new Appointment("Test2",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
-        Appointment TestAppointment3 = new Appointment("Test3",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment1 = new Appointment("Test1",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment2 = new Appointment("Test2",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment3 = new Appointment("Test3",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
         Student RandomName = new Student("Random Name","12345");
         RandomName.addAppointment(TestAppointment1,TestAppointment3);
         //when
         RandomName.delAppointment(TestAppointment1,TestAppointment2);
         //then
-        Assert.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment1","TestAppointment3"));
+        Assertions.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment1","TestAppointment3"));
 
     }
     @Test
     void TestdelSEVERALAppointments_ALLsigned () {
         //given
-        Appointment TestAppointment1 = new Appointment("Test1",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
-        Appointment TestAppointment2 = new Appointment("Test2",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
-        Appointment TestAppointment3 = new Appointment("Test3",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
-        Appointment TestAppointment4 = new Appointment("Test4",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment1 = new Appointment("Test1",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment2 = new Appointment("Test2",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment3 = new Appointment("Test3",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment TestAppointment4 = new Appointment("Test4",TestProfessor,TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
         Student RandomName = new Student("Random Name","12345");
         RandomName.addAppointment(TestAppointment1,TestAppointment2,TestAppointment3,TestAppointment4);
         //when
         RandomName.delAppointment(TestAppointment1,TestAppointment2,TestAppointment3);
         //then
-        Assert.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment4"));
+        Assertions.assertEquals(RandomName.getTermine(), Arrays.asList("TestAppointment4"));
     }
 
 
@@ -85,10 +88,10 @@ class CreatorTest {
     void TestcreateAppointment_return() { //--> addApointment mus nicht getestet werden da es bereits getestet wurde
         //given
 
-        Appointment expectedAppointment = new Appointment("Test",TestProfessor,2022-03-24,Repetition.ONCE,Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment expectedAppointment = new Appointment("Test",TestProfessor,TestTime,Repetition.ONCE,Campus.ALTONAERCAMPUS,"Test","Test Description");
         //when
-        Appointment result = TestProfessor.createAppointment("Test",TestProfessor,, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
+        Appointment result = TestProfessor.createAppointment("Test",TestTime, Repetition.ONCE, Campus.ALTONAERCAMPUS,"Test","Test Description");
         //then
-        Assert.assertEquals(result,expectedAppointment);
+        Assertions.assertEquals(result,expectedAppointment);
     }
 }
