177418485bf2bc683657b40bdb4a283f62252a37
Nadine Huetter <nadine.huetter@fh-erfurt.de>
2022-03-24 09:55:10 +0100

    unit test 2

---
+++ appointments-service/src/main/java/de/fherfurt/appointments/models/CreatorTest.java
@@ -0,0 +1,29 @@
+package de.fherfurt.appointments.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreatorTest {
+
+    @Test
+    void addAppointment() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void delAppointment() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void createAppointment() {
+        //given
+        //when
+        //then
+    }
+}
---
+++ appointments-service/src/main/java/de/fherfurt/appointments/models/ProfessorTest.java
@@ -0,0 +1,15 @@
+package de.fherfurt.appointments.models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfessorTest {
+
+    @Test
+    void informStudents() {
+        //given
+        //when
+        //then
+    }
+}
