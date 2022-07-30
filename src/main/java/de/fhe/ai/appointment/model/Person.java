bf2a7cfa235980544b6cb3b792f8f9646b7ebc0c
StephanTeichmueller <92719066+StephanTeichmueller@users.noreply.github.com>
2022-07-30 18:08:32 +0200

    Changes by vanxa

--- src/main/java/de/fhe/ai/appointment/model/Person.java
+++ src/main/java/de/fhe/ai/appointment/model/Person.java
@@ -7,7 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
+import java.util.List;
 
 
 @Getter
